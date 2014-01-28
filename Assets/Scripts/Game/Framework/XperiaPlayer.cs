63e6cef03dc10bb241900c399226c28325bc59e2
Kevin Krol <krol.kevin@gmail.com>
2014-01-28 23:47:01 +0100

    More structuring

--- Assets/Scripts/Game/Framework/XperiaPlayer.cs
+++ Assets/Scripts/Game/Framework/XperiaPlayer.cs
@@ -1,26 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class XperiaPlayer : MonoBehaviour {
+public class XPeriaPlayer:MonoBehaviour {
 	public Vector2 movement;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		movement = new Vector2(0,0);
-		if(Input.GetKey(KeyCode.UpArrow)){
+
+	void Update() {
+		movement = new Vector2(0, 0);
+		
+		if(Input.GetKey(KeyCode.UpArrow)) {
 			movement.y -= 0.1f;
-		}else if(Input.GetKey(KeyCode.DownArrow)){
+		} else if(Input.GetKey(KeyCode.DownArrow)) {
 			movement.y += 0.1f;
 		}
-		if(Input.GetKey(KeyCode.LeftArrow)){
+		
+		if(Input.GetKey(KeyCode.LeftArrow)) {
 			movement.x += 0.1f;
-		}else if(Input.GetKey(KeyCode.RightArrow)){
+		} else if(Input.GetKey(KeyCode.RightArrow)) {
 			movement.x -= 0.1f;
 		}
+		
 		transform.Translate(movement);
 	}
 }
