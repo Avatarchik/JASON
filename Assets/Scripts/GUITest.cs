a8bf90dc1789af6c41b2aeb52431e96591f17ce8
Kevin Krol <krol.kevin@gmail.com>
2014-01-29 00:42:26 +0100

    Fix

--- Assets/Scripts/GUITest.cs
+++ Assets/Scripts/GUITest.cs
@@ -5,8 +5,8 @@ public class GUITest:MonoBehaviour {
 	public Texture2D[] textures;
 
 	void Start () {
-		button = new Button(new Vector2(100, 100), textures[0], textures[1], textures[2], Button => Debug.Log ("Clicked"));
-		label = new Label(new Vector2(100, 10), new GUIContent("Fapfapfap"));
+		new Button(new Vector2(100, 100), textures[0], textures[1], textures[2], Button => Debug.Log ("Clicked"));
+		new Label(new Vector2(100, 10), new GUIContent("Fapfapfap"));
 		
 		Debug.Log(SingletonTest.Instance.I);
 	}
