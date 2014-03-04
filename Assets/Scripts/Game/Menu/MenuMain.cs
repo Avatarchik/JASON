fde8cf47f89d81047642d4e48a530de3df1ac986
kevinness <bioniclefreak_19@hotmail.com>
2014-03-04 11:25:57 +0100

    DADADAKAKAKAKAKAKAKAKAKA

--- Assets/Scripts/Game/Menu/MenuMain.cs
+++ Assets/Scripts/Game/Menu/MenuMain.cs
@@ -1,5 +1,12 @@
 ﻿using UnityEngine;
 
-public class MenuMain:MonoBehaviour {
+public class MenuMain:GUIBehaviour {
 
+
+	void OnGUI(){
+		base.OnGUI();
+		if(GUI.Button(new Rect(0,0,400,100),"Start")){
+			Application.LoadLevel("Game");
+		}
+	}
 }
---
+++ Assets/Scripts/Generic/GUI/GUIBehaviour.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIBehaviour : MonoBehaviour {
+	private Vector2 nativeSize;
+	public void OnGUI () {
+		GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(Screen.width / GUIManager.Instance.NativeSize.x, Screen.height / GUIManager.Instance.NativeSize.y, 1));
+	}
+}
